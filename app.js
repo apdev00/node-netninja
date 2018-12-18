862b655823f2cffc9c667c5f6164a5df40997428
Alexander Pasieka <AlexanderP@AlexanderP-T470s.dcrworkforce.local>
2018-12-17 21:48:41 -0500

    tutorial 25 - 20181217a

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-var stuff = require('./stuff');
+'use strict';
 
-console.log(stuff.counter(['a', 'b', 'c']));
-console.log(stuff.adder(2,3));
-console.log(stuff.pi);
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('this is the home page');
+});
+
+app.get('/contact', function(req, res){
+    res.send('this is the contact page');
+});
+
+app.get('/profile/:id', function(req, res){
+    res.send('you requested to see the profile with id: ' + req.params.id);
+});
+
+app.listen(3000);
